4ffec8058a23b19619bac78b7477d1a9a662b2aa
Walid Boudabbous <w.boudabbous@dudoxx.com>
2024-10-16 21:00:10 +0200

    Initial commit: Dudoxx Media Manager module

--- dudoxx_media/models/dudoxx_media_category.py
+++ dudoxx_media/models/dudoxx_media_category.py
@@ -4,5 +4,11 @@ class DudoxxMediaCategory(models.Model):
     _name = 'dudoxx.media.category'
     _description = 'Dudoxx Media Category'
 
-    name = fields.Char(string='Name', required=True, index=True, unique=True)
+    name = fields.Char(string='Name', required=True)
     description = fields.Text(string='Description')
+    
+    media_file_ids = fields.One2many('dudoxx.media.file', 'category_id', string='Media Files')
+
+    _sql_constraints = [
+        ('name_uniq', 'unique (name)', "Category name already exists !"),
+    ]
